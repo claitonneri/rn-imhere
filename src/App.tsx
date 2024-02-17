c4e66ec6cf3cd9ff2de5d66d6b3707f85474d73a
claitonneri <claitondidone@gmail.com>
2024-02-17 00:05:26 -0300

    chore: initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Device from 'expo-device'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>
+        {Device.manufacturer}: {Device.deviceName}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
